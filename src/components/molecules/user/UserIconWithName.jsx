9bcd6975e64d590964b77bc65c621d02871541b1
kiyo <18kyohei@gmail.com>
2025-10-09 08:35:43 +0900

    add DefalutLayout.jsx

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,9 +1,26 @@
+import styled from "styled-components";
+
 export const UserIconWithName = (props) => {
     const { image, name } = props;
     return (
-      <div>
-        <img height={160} width={160} src={image} alt="プロフィール" />
-        <p>{name}</p>
-      </div>
+      <SContainer>
+        <SImage height={160} width={160} src={image} alt="プロフィール" />
+        <SName>{name}</SName>
+      </SContainer>
     );
   };
+
+  const SContainer = styled.div`
+    text-align: center;
+  `
+
+  const SImage = styled.img`
+    border-radius: 50%;
+  `
+
+  const SName = styled.p`
+    font-size: 18px;
+    font-weight: bold;
+    margin: 0px;
+    font-color: #40514e
+    `
---
+++ src/components/template/DefalutLaout.jsx
@@ -0,0 +1,5 @@
+export const DefaultLayout = (props) => {
+  const { children } = props;
+  return <div>{children}</div>;
+};
+ 
